cdcf24b1253a1e2a7974acf2c19527e6a585150d
Manishkapoor2002 <kapoor.manish200@gmail.com>
2024-04-06 23:15:34 +0530

    Some more routes are add

--- index.js
+++ index.js
@@ -8,20 +8,20 @@ const { Schema } = mongoose;
 const Secretkey = 'FmrCJx3sdzRElzP';
 
 
-function authenticationJWT(req,res,next){
+function authenticationJWT(req, res, next) {
     const authHeader = req.headers.authentication;
-    if(authHeader){
+    if (authHeader) {
         const userToken = authHeader.split(' ')[1];
-        jwt.verify(userToken,Secretkey,(err,data)=>{
-            if(err){
-                res.status(403).json({'msg':'User authorization failed!!!'});
-            }else{
+        jwt.verify(userToken, Secretkey, (err, data) => {
+            if (err) {
+                res.status(403).json({ 'msg': 'User authorization failed!!!' });
+            } else {
                 req.user = data;
                 next();
             }
-        }) 
-    }else{
-        res.status(403).json({'msg':'Login or signUp again'});
+        })
+    } else {
+        res.status(403).json({ 'msg': 'Login or signUp again' });
     }
 }
 
@@ -30,118 +30,226 @@ app.use(express.json())
 // Schemas:
 
 const userSchema = new Schema({
-    "username":String,
-    "password":String,
-    'email':String,
-    'phoneNumber':Number,
-    'age':Number,
-    'posts':[{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
-    'followings':[{ type: mongoose.Schema.Types.ObjectId, ref: 'Following' }],
-    'followers':[{ type: mongoose.Schema.Types.ObjectId, ref: 'Follower' }]
+    "username": String,
+    "password": String,
+    'email': String,
+    'phoneNumber': Number,
+    "PrivateAccount": Boolean,
+    'age': Number,
+    'posts': [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
+    'followings': [{ type: mongoose.Schema.Types.ObjectId, ref: 'Following' }],
+    'followers': [{ type: mongoose.Schema.Types.ObjectId, ref: 'Follower' }]
 })
 
 const postSchema = new Schema({
-  'imageURL':String,
-  'postBody':String,
-  'likes':[{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  'comments':[{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
-  'privatePost':Boolean,
+    'imageURL': String,
+    'postBody': String,
+    'likes': [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    'comments': [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
+    'privatePost': Boolean,
 })
 const followingSchema = new Schema({
-    'username':String,
-    'date':Date,
+    'username': String,
+    'followedBy': String,
+    'date': Date,
 })
 const followerSchema = new Schema({
-    'username':String,
-    'data':Date
+    'username': String,
+    'followeTo': String,
+    'data': Date
 })
 
 const commentSchema = new Schema({
-    'username':String,
-    'commentBody':String,
-    'likes':[{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]    
+    'username': String,
+    'commentBody': String,
+    'likes': [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
 })
 
 
 
-const User = mongoose.model('User',userSchema);
-const Post = mongoose.model('Post',postSchema);
-const Following = mongoose.model('Following',followingSchema);
-const Follower = mongoose.model('Follower',followerSchema);
-const Comment = mongoose.model('Comment',commentSchema)
+const User = mongoose.model('User', userSchema);
+const Post = mongoose.model('Post', postSchema);
+const Following = mongoose.model('Following', followingSchema);
+const Follower = mongoose.model('Follower', followerSchema);
+const Comment = mongoose.model('Comment', commentSchema)
 mongoose.connect('mongodb://localhost:27017/')
 
 
 
-app.post('/signup',async(req,res)=>{
-    const {username,password,email} = req.body;
-    const userNameverify = await User.findOne({username})
-    const userEmailVerify = await User.findOne({email})
-    if(userNameverify){
-        res.status(403).json({"msg":'UerName Already Exist'})
-    }else if(userEmailVerify){
-        res.status(403).json({"msg":'Email Already Exist'})
-    }else{
-        const token = jwt.sign({'username':username,'email':email},Secretkey,{expiresIn:'7days'})
-        console.log(username+" <-> " +email +" <-> " +password + " <-> " +token)
-        const newUser = new User({'username':username,'password':password,'email':email})
+app.post('/signup', async (req, res) => {
+    const { username, password, email } = req.body;
+    const userNameverify = await User.findOne({ username })
+    const userEmailVerify = await User.findOne({ email })
+    if (userNameverify) {
+        res.status(403).json({ "msg": 'UerName Already Exist' })
+    } else if (userEmailVerify) {
+        res.status(403).json({ "msg": 'Email Already Exist' })
+    } else {
+        const token = jwt.sign({ 'username': username, 'email': email }, Secretkey, { expiresIn: '7days' })
+        console.log(username + " <-> " + email + " <-> " + password + " <-> " + token)
+        const newUser = new User({ 'username': username, 'password': password, 'email': email, 'PrivateAccount': false })
         await newUser.save();
-        res.status(200).json({'msg':'User has been created',"token":token})
+        res.status(200).json({ 'msg': 'User has been created', "token": token })
     }
 })
 
-app.post('/login',async (req,res)=>{
-   const {username,password} = req.body;
-   const userNameverify = await User.findOne({username , password})
-//    const userEmailVerify = await User.findOne({email,password})
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const userNameverify = await User.findOne({ username, password })
+    //    const userEmailVerify = await User.findOne({email,password})
 
-   if( userNameverify){
-    const userEmail = userNameverify.email;
-    const token = jwt.sign({'username':username,'email':userEmail},Secretkey,{expiresIn:'7days'})
-    console.log(username+" <-> " +userEmail +" <-> " +password + " <-> " +token)
-    res.status(200).json({'msg':'User has been successfully logged in '})
-   }else{
-    res.status(403).json({'msg':'User authorization failed'});
-   }
-  
+    if (userNameverify) {
+        const userEmail = userNameverify.email;
+        const token = jwt.sign({ 'username': username, 'email': userEmail }, Secretkey, { expiresIn: '7days' })
+        console.log(username + " <-> " + userEmail + " <-> " + password + " <-> " + token)
+        res.status(200).json({ 'msg': 'User has been successfully logged in ' })
+    } else {
+        res.status(403).json({ 'msg': 'User authorization failed' });
+    }
+
 })
 
-app.post('/UploadPost',authenticationJWT,async ( req,res)=>{
-    const {imageURL,postBody} = req.body;
-    const currUser = await User.findOne({'username':req.user.username})
+app.post('/UploadPost', authenticationJWT, async (req, res) => {
+    const { imageURL, postBody } = req.body;
+    const currUser = await User.findOne({ 'username': req.user.username })
     console.log(req.user.username)
-    if(currUser){
-    // currUser.posts.map(async(val)=>{
-    //     const temp = await Post.findById(val._id)
-    //     console.log(temp.imageURL)
-    // })
+    if (currUser) {
+        // currUser.posts.map(async(val)=>{
+        //     const temp = await Post.findById(val._id)
+        //     console.log(temp.imageURL)
+        // })
 
-    if(imageURL.length <3){
+        if (imageURL.length < 3) {
             res.status(401).json("Image length is too short or image is not found")
-    }else{
-        const newPost = new Post({
-            imageURL,
-            'privatePost':false,
-            postBody
-        })
-        await newPost.save();
-    
-        currUser.posts.push(newPost);
-        await currUser.save();
-        
-        res.status(200).json({"msg":"post uploaded successfully!"});    
+        } else {
+            const newPost = new Post({
+                imageURL,
+                'privatePost': false,
+                postBody
+            })
+            await newPost.save();
+
+            currUser.posts.push(newPost);
+            await currUser.save();
+
+            res.status(200).json({ "msg": "post uploaded successfully!" });
+        }
+    } else {
+        res.status(403).json({ "msg": "No User find!!" })
     }
-}else{
-    res.status(403).json({"msg":"No User find!!"})
-}
-    
+
 })
 
-app.get('/',(req,res)=>{
+app.get('/profile/:username', authenticationJWT, async (req, res) => {
+    const user = await User.findOne({ 'username': req.params.username });
+
+    if (user) {
+        res.status(200).json({
+            "username": user.username,
+            "email": user.email,
+            "posts": user.posts,
+            "followings": user.followings,
+            "followers": user.followers
+        });
+    } else {
+        res.status(403).json({ "msg": "User not found" })
+    }
+})
+
+
+app.post('/follow/:username', authenticationJWT, async (req, res) => {
+    const otherUser = await User.findOne({ 'username': req.params.username });
+    const currUser = await User.findOne({ "username": req.user.username })
+
+    if (currUser) {
+        if (otherUser) {
+            // already following or not!!   
+           
+            const check = await User.findOne({ 'username': currUser.username });
+            const checkArray = check.followings;
+            const verify = checkArray.map(val => val.username === otherUser.username);
+            
+            if (otherUser.username == currUser.username) {
+                res.status(203).json({ "msg": "following yourself is not possible!!" })
+            }else  if(verify){
+                res.status(403).json({"msg":"You are already follows the "+req.params.username})
+            }else {
+                var currentDate = currDateWithTime();;
+
+                const newFollower = new Follower({
+                    "username": currUser.username,
+                    "followeTo": otherUser.username,
+                    'date': currentDate
+                })
+
+                const newFollowing = new Following({
+                    "username": otherUser.username,
+                    "followedBy": currUser.username,
+                    'date': currentDate
+                })
+
+                await newFollower.save();
+                await newFollowing.save();
+                currUser.followings.push(newFollowing);
+                await currUser.save();
+                otherUser.followers.push(newFollower);
+                await otherUser.save();
+
+                res.status(200).json({ "msg": "followed successfully!!" });
+            }
+
+
+        } else {
+            res.status(403).json({ "msg": "User not found!!" })
+        }
+    } else {
+        res.status(403).json({ "msg": "Please re login!" })
+    }
+})
+
+app.get('/followings/:username',authenticationJWT, async (req,res)=>{
+    const user =  await User.findOne({"username":req.params.username}).populate('followings');
+    if(user){
+        res.status(200).json({"Following" : user.followings || []})
+    }else{
+        console.log("admjshb")
+        res.status(403).json("User Not Found!!")
+    }
+})
+
+app.get('/followers/:username',authenticationJWT,async (req,res)=>{
+    const user =  await User.findOne({"username":req.params.username}).populate('followers');;
+    if(user){
+        res.status(200).json({"Following" : user.followers || []})
+    }else{
+        console.log("admjshb")
+        res.status(403).json("User Not Found!!")
+    }
+})
+
+app.get('/', (req, res) => {
     res.send("Manish kapoor")
 })
 
 
-app.listen(port,()=>{
-    console.log("successfuly listening on Port number " +port)
+app.listen(port, () => {
+    console.log("successfuly listening on Port number " + port)
 })
+
+
+
+function currDateWithTime() {
+    var currentDate = new Date();
+
+    var year = currentDate.getFullYear();
+    var month = currentDate.getMonth() + 1;
+    var day = currentDate.getDate();
+    var hour = currentDate.getHours();
+    var minute = currentDate.getMinutes();
+    var second = currentDate.getSeconds();
+
+    var formattedDate = year + "-" + month + "-" + day;
+    var formattedTime = hour + ":" + minute + ":" + second;
+    return (formattedDate + " " + formattedTime);
+
+}
